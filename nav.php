4c0413b2762077fa8a3ac2d62da62e01b6357674
mik4ra <mik4ra@gmail.com>
2021-10-14 16:24:32 +0800

    First Update
    Update of 5 initial files

---
+++ nav.php
@@ -0,0 +1,7 @@
+<div id="banner"><img src="banner.jpg" class="center"></div>
+<div class="topnav">
+ <a href="logcall.php">Log Call</a>
+ <a href="update.php">Update</a>
+ <a href="#">Report</a>
+ <a href="#">History</a>
+</div>
